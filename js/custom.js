5c27cc516d5c062b7d9a15fd45db7b6fd4974f3a
desipossa <c1jang@hanmail.net>
2024-11-07 16:58:31 +0900

    20241107

---
+++ js/custom.js
@@ -0,0 +1,22 @@
+$(function () {
+    const MVS = new Swiper('.main_visual_slide', {
+        loop: true,
+        grabCursor: true,
+        effect: "creative",
+        creativeEffect: {
+            prev: {
+                shadow: true,
+                translate: ["-20%", 0, -1],
+            },
+            next: {
+                translate: ["100%", 0, 0],
+            },
+        },
+        speed: 800,
+        autoplay: {
+            delay: 4000,
+            disableOnInteraction: false,
+
+        },
+    })
+})
