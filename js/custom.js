b115d2326cbbedf7afaf75de87867a56e5737575
desipossa <c1jang@hanmail.net>
2024-11-12 12:30:48 +0900

    20241112

--- js/custom.js
+++ js/custom.js
@@ -23,5 +23,39 @@ $(function () {
             el: '#main_visual .page',
             clickable: true,
         },
+    });
+
+
+    const MCS = new Swiper('.main_content_slide', {
+        loop: true,
+        grabCursor: true,
+        slidesPerView: 1,
+        spaceBetween: 30,
+        speed: 800,
+        autoplay: {
+            delay: 4000,
+            disableOnInteraction: false,
+
+        },
+
+        pagination: {
+            el: '#main_content01 .page',
+            clickable: true,
+        },
+
+        breakpoints: {
+            640: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+            768: {
+                slidesPerView: 3,
+                spaceBetween: 20,
+            },
+            1024: {
+                slidesPerView: 4.5,
+                spaceBetween: 30,
+            },
+        },
     })
 })
